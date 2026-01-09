e4c5bacebeeed6f21fed7f7871c9a9c3914c0557
Bongani Masuku <bongani@1702tech.com>
2026-01-09 16:39:25 +0200

    build request controller

---
+++ services/alchemypdf.api.app/app/controller/requestctrl/requestctrl.go
@@ -0,0 +1,297 @@
+package requestctrl
+
+import (
+	"fmt"
+	"time"
+
+	constant "alchemypdf.api/lib/alchemypdf.api.constant"
+	"alchemypdf.api/lib/alchemypdf.api.contract/requestcontract"
+	"alchemypdf.api/lib/alchemypdf.api.infrastructure/config"
+	"alchemypdf.api/lib/alchemypdf.api.infrastructure/loglocal"
+	"alchemypdf.api/lib/alchemypdf.api.service/requestsvc"
+	"alchemypdf.api/lib/alchemypdf.api.util/errorlocal"
+	"alchemypdf.api/lib/alchemypdf.api.util/httpresponse"
+	"alchemypdf.api/lib/alchemypdf.api.util/httpstatuslocal"
+	"github.com/labstack/echo/v4"
+)
+
+type (
+	IRequestCtrl interface {
+		HandleCreate(c echo.Context) error
+		HandleGetByClientReference(c echo.Context) error
+		HandleGetWithContentByClientReference(c echo.Context) error
+		HandleGetPending(c echo.Context) error
+		HandleComplete(c echo.Context) error
+	}
+
+	RequestCtrl struct {
+		config         config.IConfig
+		logger         loglocal.ILogger
+		requestService requestsvc.IRequestService
+	}
+
+	CreateRequest struct {
+		ClientReference string `json:"clientReference"`
+		Content         string `json:"content"`
+		CallbackURL     string `json:"callbackUrl"`
+	}
+
+	Request struct {
+		RequestID         int    `json:"requestId"`
+		RequestExternalID string `json:"requestExternalId"`
+		RequestStateKey   string `json:"requestStateKey"`
+
+		ClientReference string `json:"clientReference"`
+		Content         string `json:"content"`
+		CallbackURL     string `json:"callbackUrl"`
+		AttemptCount    int    `json:"attemptCount"`
+
+		CreatedAt time.Time `json:"createdAt"`
+		UpdatedAt time.Time `json:"updatedAt"`
+	}
+
+	CompleteRequest struct {
+		Success   bool `json:"success"`
+		RequestID int  `json:"requestId"`
+	}
+)
+
+func NewRequestCtrl(
+	config config.IConfig,
+	logger loglocal.ILogger,
+	requestService requestsvc.IRequestService,
+) *RequestCtrl {
+	return &RequestCtrl{
+		config,
+		logger,
+		requestService,
+	}
+}
+
+// Request/Create godoc
+// @id Request.Create
+// @tags Request
+// @summary Create a request for converting HTML to PDF
+// @router /v1/Request/Create [post]
+// @accept json
+// @produce json
+// @success 200 {object} httpresponse.Response
+// @Failure 400,500 {object} httpresponse.Response
+// @param x-api-key header string true "API Key"
+// @param payload body CreateRequest true "CreateRequest"
+func (ctrl RequestCtrl) HandleCreate(c echo.Context) error {
+	const op = "RequestCtrl.HandleCreate"
+
+	ctrl.logger.Info(fmt.Sprintf("[%s]: starting", op))
+
+	var req CreateRequest
+	if err := c.Bind(&req); err != nil {
+		ctrl.logger.Debug(`"RequestCtrl.HandleCreate": failed to parse body`)
+		ctrl.logger.AppError(errorlocal.UnknownErr(err.Error(), op, err))
+
+		statusCode, _ := httpstatuslocal.InternalServerError()
+		ctrl.logger.HTTPResponse(statusCode, c.Request().Method, c.Request().RequestURI, fmt.Sprint(c.Get(ctrl.config.RequestIDKey())))
+		return c.JSON(statusCode, httpresponse.Default(err.Error(), statusCode))
+	}
+
+	if err := ctrl.requestService.Create(requestcontract.CreateRequest{
+		ClientReference: req.ClientReference,
+		Content:         req.Content,
+		CallbackURL:     req.CallbackURL,
+	}); err != nil {
+		ctrl.logger.AppError(errorlocal.CatchErr(err, op))
+		ctrl.logger.HTTPResponse(err.StatusCode(), c.Request().Method, c.Request().RequestURI, fmt.Sprint(c.Get(ctrl.config.RequestIDKey())))
+		return c.JSON(err.StatusCode(), httpresponse.Default(err.Error(), err.StatusCode()))
+	}
+
+	statusCode, statusCodeText := httpstatuslocal.Ok()
+
+	ctrl.logger.HTTPResponse(statusCode, c.Request().Method, c.Request().RequestURI, fmt.Sprint(c.Get(ctrl.config.RequestIDKey())))
+
+	return c.JSON(statusCode, httpresponse.Default(statusCodeText, statusCode))
+}
+
+// Request/GetByClientReference godoc
+// @id Request.GetByClientReference
+// @tags Request
+// @summary Gets a list of request created by the client without content
+// @router /v1/Request/GetByClientReference/{ClientReference} [get]
+// @accept json
+// @produce json
+// @success 200 {object} []Request
+// @Failure 400,500 {object} httpresponse.Response
+// @param x-api-key header string true "API Key"
+// @param ClientReference path string true "ClientReference"
+func (ctrl RequestCtrl) HandleGetByClientReference(c echo.Context) error {
+	const op = "RequestCtrl.HandleGetByClientReference"
+
+	ctrl.logger.Info(fmt.Sprintf("[%s]: starting", op))
+
+	res, err := ctrl.requestService.GetByClientReference(c.Param("ClientReference"))
+	if err != nil {
+		ctrl.logger.AppError(errorlocal.CatchErr(err, op))
+		return c.JSON(err.StatusCode(), httpresponse.Default(err.Error(), err.StatusCode()))
+	}
+
+	statusCode, _ := httpstatuslocal.Ok()
+
+	ctrl.logger.HTTPResponse(statusCode, c.Request().Method, c.Request().RequestURI, fmt.Sprint(c.Get(ctrl.config.RequestIDKey())))
+
+	return c.JSON(statusCode, mapListToDomain(res))
+}
+
+// Request/GetWithContentByClientReference godoc
+// @id Request.GetWithContentByClientReference
+// @tags Request
+// @summary Gets a list of request created by the client with content
+// @router /v1/Request/GetWithContentByClientReference/{ClientReference} [get]
+// @accept json
+// @produce json
+// @success 200 {object} []Request
+// @Failure 400,500 {object} httpresponse.Response
+// @param x-api-key header string true "API Key"
+// @param ClientReference path string true "ClientReference"
+func (ctrl RequestCtrl) HandleGetWithContentByClientReference(c echo.Context) error {
+	const op = "RequestCtrl.HandleGetWithContentByClientReference"
+
+	ctrl.logger.Info(fmt.Sprintf("[%s]: starting", op))
+
+	res, err := ctrl.requestService.GetWithContentByClientReference(c.Param("ClientReference"))
+	if err != nil {
+		ctrl.logger.AppError(errorlocal.CatchErr(err, op))
+		return c.JSON(err.StatusCode(), httpresponse.Default(err.Error(), err.StatusCode()))
+	}
+
+	statusCode, _ := httpstatuslocal.Ok()
+
+	ctrl.logger.HTTPResponse(statusCode, c.Request().Method, c.Request().RequestURI, fmt.Sprint(c.Get(ctrl.config.RequestIDKey())))
+
+	return c.JSON(statusCode, mapListToDomain(res))
+}
+
+// Request/GetPending godoc
+// @id Request.GetPending
+// @tags Request
+// @summary Gets a list of request created by the client with content
+// @router /v1/Request/GetPending [get]
+// @accept json
+// @produce json
+// @success 200 {object} Request
+// @Failure 400,500 {object} httpresponse.Response
+// @param x-api-key header string true "API Key"
+func (ctrl RequestCtrl) HandleGetPending(c echo.Context) error {
+	const op = "RequestCtrl.HandleGetPending"
+
+	ctrl.logger.Info(fmt.Sprintf("[%s]: starting", op))
+
+	res, err := ctrl.getPending()
+	if err != nil {
+		ctrl.logger.AppError(errorlocal.CatchErr(err, op))
+		return c.JSON(err.StatusCode(), httpresponse.Default(err.Error(), err.StatusCode()))
+	}
+
+	if err := ctrl.requestService.StateUpdate(requestcontract.StateUpdateRequest{
+		RequestID:       res.RequestID,
+		RequestStateKey: constant.RequestStateInProgress,
+	}); err != nil {
+		ctrl.logger.AppError(errorlocal.CatchErr(err, op))
+		return c.JSON(err.StatusCode(), httpresponse.Default(err.Error(), err.StatusCode()))
+	}
+
+	statusCode, _ := httpstatuslocal.Ok()
+
+	ctrl.logger.HTTPResponse(statusCode, c.Request().Method, c.Request().RequestURI, fmt.Sprint(c.Get(ctrl.config.RequestIDKey())))
+
+	return c.JSON(statusCode, res)
+}
+
+func mapListToDomain(l []*requestcontract.Request) []*Request {
+	r := []*Request{}
+
+	for _, v := range l {
+		r = append(r, mapToDomain(v))
+	}
+
+	return r
+}
+
+func mapToDomain(v *requestcontract.Request) *Request {
+	return &Request{
+		RequestID:         v.RequestID,
+		RequestExternalID: v.RequestExternalID,
+		RequestStateKey:   v.RequestStateKey,
+		ClientReference:   v.ClientReference,
+		Content:           v.Content,
+		CallbackURL:       v.CallbackURL,
+		AttemptCount:      v.AttemptCount,
+		CreatedAt:         v.CreatedAt,
+		UpdatedAt:         v.UpdatedAt,
+	}
+}
+
+func (ctrl RequestCtrl) getPending() (*Request, *errorlocal.AppError) {
+	const op = "RequestCtrl.getPending"
+
+	ctrl.logger.Info(fmt.Sprintf("[%s]: starting", op))
+
+	res, err := ctrl.requestService.GetByState(constant.RequestStatePending)
+	if err != nil {
+		return nil, errorlocal.CatchErr(err, op)
+	}
+
+	if res.AttemptCount >= 3 {
+		if err := ctrl.requestService.StateUpdate(requestcontract.StateUpdateRequest{
+			RequestID:       res.RequestID,
+			RequestStateKey: constant.RequestStateAttemptLimitReached,
+		}); err != nil {
+			return nil, errorlocal.CatchErr(err, op)
+		}
+
+		return ctrl.getPending()
+	}
+
+	return mapToDomain(res), nil
+}
+
+// Request/Complete godoc
+// @id Request.Complete
+// @tags Request
+// @summary Completes a request, sets it to complete if successful, pending if failed
+// @router /v1/Request/Complete [post]
+// @accept json
+// @produce json
+// @success 200 {object} httpresponse.Response
+// @Failure 400,500 {object} httpresponse.Response
+// @param x-api-key header string true "API Key"
+// @param payload body CompleteRequest true "CompleteRequest"
+func (ctrl RequestCtrl) HandleComplete(c echo.Context) error {
+	const op = "RequestCtrl.HandleComplete"
+
+	ctrl.logger.Info(fmt.Sprintf("[%s]: starting", op))
+
+	var completeReq CompleteRequest
+	if err := c.Bind(&completeReq); err != nil {
+		ctrl.logger.Debug(fmt.Sprintf("[%s]: failed to parse body", op))
+		ctrl.logger.AppError(errorlocal.UnknownErr(err.Error(), op, err))
+
+		statusCode, _ := httpstatuslocal.InternalServerError()
+		ctrl.logger.HTTPResponse(statusCode, c.Request().Method, c.Request().RequestURI, fmt.Sprint(c.Get(ctrl.config.RequestIDKey())))
+		return c.JSON(statusCode, httpresponse.Default(err.Error(), statusCode))
+	}
+
+	requestStateKey := constant.RequestStatePending
+	if completeReq.Success {
+		requestStateKey = constant.RequestStateCompleted
+	}
+
+	if err := ctrl.requestService.StateUpdate(requestcontract.StateUpdateRequest{
+		RequestID:       completeReq.RequestID,
+		RequestStateKey: requestStateKey,
+	}); err != nil {
+		ctrl.logger.AppError(errorlocal.CatchErr(err, op))
+		return c.JSON(err.StatusCode(), httpresponse.Default(err.Error(), err.StatusCode()))
+	}
+
+	statusCode, statusCodeText := httpstatuslocal.Ok()
+	return c.JSON(statusCode, httpresponse.Default(statusCodeText, statusCode))
+}
