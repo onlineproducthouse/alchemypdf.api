e5f9227cd94c22f78ddf104ca310ac79c480e829
Bongani Masuku <bongani@1702tech.com>
2026-01-09 16:39:16 +0200

    build health check controller

---
+++ services/alchemypdf.api.app/app/controller/healthcheckctrl/healthcheckctrl.go
@@ -0,0 +1,47 @@
+package healthcheckctrl
+
+import (
+	"fmt"
+
+	"alchemypdf.api/lib/alchemypdf.api.infrastructure/config"
+	"alchemypdf.api/lib/alchemypdf.api.infrastructure/loglocal"
+	"alchemypdf.api/lib/alchemypdf.api.service/healthchecksvc"
+	"alchemypdf.api/lib/alchemypdf.api.util/httpresponse"
+	"alchemypdf.api/lib/alchemypdf.api.util/httpstatuslocal"
+	"github.com/labstack/echo/v4"
+)
+
+type HealthCheck struct {
+	logger loglocal.ILogger
+	hc     healthchecksvc.IHealthCheckService
+	config config.IConfig
+}
+
+type IHealthCheck interface {
+	Ping(c echo.Context) error
+}
+
+func New(logger loglocal.ILogger, hc healthchecksvc.IHealthCheckService, config config.IConfig) HealthCheck {
+	return HealthCheck{logger, hc, config}
+}
+
+// HealthCheck/Ping godoc
+// @id HealthCheck.Ping
+// @tags HealthCheck
+// @summary Gets the health status of server.
+// @router /HealthCheck/Ping [get]
+// @accept */*
+// @produce json
+// @success 200 {object} httpresponse.Response
+// @Failure 500 {object} httpresponse.Response
+func (ctrl HealthCheck) Ping(c echo.Context) error {
+
+	if err := ctrl.hc.Ping(); err != nil {
+		ctrl.logger.Fatal(err.Error())
+	}
+
+	statusCode, statusCodeText := httpstatuslocal.Ok()
+	ctrl.logger.HTTPResponse(statusCode, c.Request().Method, c.Request().RequestURI, fmt.Sprint(c.Get(ctrl.config.RequestIDKey())))
+
+	return c.JSON(statusCode, httpresponse.Default(statusCodeText, statusCode))
+}
