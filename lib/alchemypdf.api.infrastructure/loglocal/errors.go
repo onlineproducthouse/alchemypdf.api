267e96b51ed055b78ec96f597e230df051e112f3
Bongani Masuku <bongani@1702tech.com>
2026-01-09 16:33:34 +0200

    build logging service

---
+++ lib/alchemypdf.api.infrastructure/loglocal/errors.go
@@ -0,0 +1,23 @@
+package loglocal
+
+import (
+	"strings"
+
+	constant "alchemypdf.api/lib/alchemypdf.api.constant"
+	"alchemypdf.api/lib/alchemypdf.api.util/errorlocal"
+	"github.com/rs/zerolog/log"
+)
+
+func (logger Logger) AppError(err errorlocal.IAppError) {
+	if strings.ToLower(logger.config.EnvName()) == constant.ENV_LOCAL ||
+		strings.ToLower(logger.config.EnvName()) == constant.ENV_TEST {
+		log.Err(err)
+	}
+
+	log.Error().
+		Str("kind", err.Kind()).
+		Int("httpStatusCode", err.StatusCode()).
+		// Str("innerMessage", err.Trace().InnerMessage).
+		Str("trace", strings.Join(err.Trace().Ops, ", ")).
+		Msg(err.Error())
+}
