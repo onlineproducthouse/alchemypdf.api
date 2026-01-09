2cf85f19acb4e2da10d9375da169009d1a9a3ea1
Bongani Masuku <bongani@1702tech.com>
2026-01-09 16:33:02 +0200

    add constants

---
+++ lib/alchemypdf.api.constant/environment.go
@@ -0,0 +1,8 @@
+package constant
+
+const (
+	ENV_LOCAL = "local"
+	ENV_TEST  = "test"
+	ENV_QA    = "qa"
+	ENV_PROD  = "prod"
+)
---
+++ lib/alchemypdf.api.constant/request.go
@@ -0,0 +1,8 @@
+package constant
+
+const (
+	RequestStatePending             string = "Pending"
+	RequestStateInProgress          string = "InProgress"
+	RequestStateAttemptLimitReached string = "AttemptLimitReached"
+	RequestStateCompleted           string = "Completed"
+)
